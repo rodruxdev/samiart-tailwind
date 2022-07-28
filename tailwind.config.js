a3d782d317fea81a73823863faa86da5ba758844
rodrigojgq <rodrigojgq@gmail.com>
2022-07-28 11:21:47 -0400

    ✨Add small animation in cards
    Add tailwind minify script in package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
   darkMode: "class",
   theme: {
     extend: {
